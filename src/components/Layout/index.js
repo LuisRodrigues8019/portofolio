58eced783f640fc470493cddf93e30d3c3e759ba
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2022-10-11 16:06:09 +0200

    layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,23 @@
-import { Outlet } from 'react-router-dom';
-import './index.scss';
-import Sidebar from '../Sidebar';
-
+import { Outlet } from 'react-router-dom'
+import './index.scss'
+import Sidebar from '../Sidebar'
 
 const Layout = () => {
-    return (
-    <div className='App'>
-    <Sidebar/>
-    <div className='page'>
-        <span>
-            
-        </span>
+  return (
+    <div className="App">
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+        <Outlet />
+
+        <span className="tags bottom-tags">
+            &lt;/body&gt;
+            <br/>
+            <span className='bottom-tag-html'>&lt;/html&gt;</span>
+            </span>
+      </div>
     </div>
-    </div>
-    )
+  )
 }
 
-export default Layout 
+export default Layout
