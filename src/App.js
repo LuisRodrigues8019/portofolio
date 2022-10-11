de4cbce6521db93761492b950679bd7fa78cfe67
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2022-10-11 12:47:32 +0200

    installation

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Layout/>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
