59e695be4e88a23bf4ecc816446b6aa9209e0096
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2022-12-27 16:14:36 +0100

    animation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,27 +1,83 @@
 import './index.scss'
+import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import { Link } from 'react-router-dom'
+import Resume from '../../assets/images/RodriguesLuisResume.pdf'
+import Loader from 'react-loaders'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faNodeJs,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    return (
-    <div className='container about-page'>
-        <div className='text-zone'>
-            <h1>
-            <AnimatedLetters 
-                strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                idx={15}
-            />
-            </h1>
-            <p>Passionate about the web, I started a professional 
-            retraining by following a Junior web developer training at Becode.
-            </p>
-            <p>
-            Organized and dynamic, I also have a seriousness and rigor 
-            that have always helped me adapt with a lot of versatility. 
-            </p>
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+            idx={15}
+          />
+        </h1>
+        <p>
+          Passionate about the web, I started a professional retraining by
+          following a Junior web developer training at Becode.
+        </p>
+        <p>
+          Organized and dynamic, I also have a seriousness and rigor that have
+          always helped me adapt with a lot of versatility.
+        </p>
+        <Link
+          to={Resume}
+          target="_blank"
+          without
+          rel="noreferrer"
+          className="flat-button"
+        >
+          DOWNLOAD RESUME
+        </Link>
+      </div>
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faNodeJs} color="#75AD64" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+          </div>
         </div>
+      </div>
     </div>
-    )
+    <Loader type='pacman' />
+    </>
+  )
 }
 
-export default About;
+export default About
