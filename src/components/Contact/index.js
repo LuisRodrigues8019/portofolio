6ea77c3d169930d86cf7b5eaf187ab8078fdfbdf
Rodrigues da Silva <rodriguesdasilva@MacBook-Pro-de-Rodrigues.local>
2023-01-02 13:53:12 +0100

    index change

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,15 +2,40 @@ import './index.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
+import Logo2 from './Logo2'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'gmail',
+        'gmailtemplate',
+        form.current,
+        'user_JVzgAzWIyQO5rW05njWQE'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
 
   return (
     <>
@@ -29,27 +54,42 @@ const Contact = () => {
             to get in touch, do not hesitate to contact me using the form below.
           </p>
           <div className="contact-form">
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
-                  <input type="name" name="name" placeholder="Name" required />
+                  <input placeholder="Name" type="text" name="name" required />
                 </li>
                 <li className="half">
-                  <input type="email" name="email" placeholder="Email" required />
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
                 </li>
                 <li>
-                    <input placeholder="Subject" type="text" name="subject" required />
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
                 </li>
                 <li>
-                    <textarea placeholder="Message" name="message" required></textarea>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
                 </li>
                 <li>
-                    <input type="submit" className="flat-button" value="SEND" />
+                  <input type="submit" className="flat-button" value="SEND" />
                 </li>
               </ul>
             </form>
           </div>
         </div>
+        <Logo2 />
       </div>
       <Loader type="pacman" />
     </>
